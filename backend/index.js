ed2dcab8414f4418fae2a5bc7e9468bafe7db3e7
imdhruvkumar <dk0495682@gmail.com>
2026-06-09 13:23:03 +0530

    Creating MERN full satck Project (backend implementation )

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db.js");
+dotenv.config();
+connectDB();
+const app = express();
+app.use(cors());
+
+app.get("/", (req,res)=>{
+    res.send("shopmore backend working properly")
+});
+
+const PORT = process.env.PORT || 5000;
+ app.listen(PORT,()=>{
+    console.log(`server is running on port ${PORT}`)
+ });
