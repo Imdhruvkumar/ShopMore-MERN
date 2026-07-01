e83ef60fd868f43117b5780e149ce88d315be098
imdhruvkumar <dk0495682@gmail.com>
2026-07-01 21:35:06 +0530

    Error debuging for problem of casesensitive deployement file controllers

--- backend/routes/orderRouter.js
+++ backend/routes/orderRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {protect} = require("../middleware/authMiddleware.js");
 const {admin} = require("../middleware/adminMiddleware.js");
-const {createOrder,getOrder,myOrders,updateOrderStatus} = require("../controller/ordercontrollers.js");
+const {createOrder,getOrder,myOrders,updateOrderStatus} = require("../controller/orderControllers.js");
 
 const router = express.Router();
 
