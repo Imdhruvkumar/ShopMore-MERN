31a05bdfd2f2ecefd90aa4ee264e372208a41266
imdhruvkumar <dk0495682@gmail.com>
2026-07-01 22:35:23 +0530

    Fix production server

--- backend/index.js
+++ backend/index.js
@@ -3,41 +3,45 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db.js");
 const path = require("path");
+
 dotenv.config();
 connectDB();
 
 const app = express();
+
 app.use(cors({
-  origin: process.env.NODE_ENV === "production"
-    ? "https://YOUR-FRONTEND-URL.onrender.com"
-    : "http://localhost:5173",
+  origin:
+    process.env.NODE_ENV === "production"
+      ? true
+      : "http://localhost:5173",
   credentials: true,
 }));
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req,res)=>{
-    res.send("shopmore backend working properly")
-});
-app.use('/api/auth', require('./routes/authRoutes.js'));
-app.use('/api/product',require('./routes/productRoutes.js'));
-app.use('/api/order',require('./routes/orderRouter.js'));
-app.use('/api/payment',require('./routes/paymentRouter.js'));
-app.use('/api/analytics',require('./routes/analyticsRouter.js'));
+// API Routes
+app.use("/api/auth", require("./routes/authRoutes.js"));
+app.use("/api/product", require("./routes/productRoutes.js"));
+app.use("/api/order", require("./routes/orderRouter.js"));
+app.use("/api/payment", require("./routes/paymentRouter.js"));
+app.use("/api/analytics", require("./routes/analyticsRouter.js"));
 
-if (process.env.NODE_ENV === 'production') {
+// Production
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.use((req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+  app.use((req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 } else {
-  app.get('/', (req, res) => {
-    res.send('ShopNest API is running in Development mode...');
+  app.get("/", (req, res) => {
+    res.send("ShopMore Backend Working Properly");
   });
 }
 
 const PORT = process.env.PORT || 5000;
- app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`)
- });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
